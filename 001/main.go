7b2c0ed30f36f447487c211eaabda4afa6c34b2f
Flemming Mahler <flemming@mahler.io>
2021-01-23 18:04:55 +0100

    Solution for problem 001

---
+++ 001/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+// Find the sum of all the multiples of 3 or 5 below 1000.
+
+func main() {
+	var sum int
+	for x := 1; x < 1000; x++ {
+		if x%3 == 0 || x%5 == 0 {
+			sum += x
+		}
+	}
+	fmt.Println("Sum:", sum)
+}
