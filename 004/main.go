10a47d378053267606035a007f27837e3ef0411f
Flemming Mahler <flemming@mahler.io>
2021-01-24 11:34:50 +0100

    Added comment

--- 004/main.go
+++ 004/main.go
@@ -5,6 +5,10 @@ import (
 	"strconv"
 )
 
+// A palindromic number reads the same both ways.
+// The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+// Find the largest palindrome made from the product of two 3-digit numbers.
+
 func main() {
 	largestPalindrome := 0
 	numberOne := 0
