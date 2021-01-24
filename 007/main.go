3496291adc6677546962b650f88bc685f71e8620
Flemming Mahler <flemming@mahler.io>
2021-01-24 13:23:32 +0100

    Solved problem 7

---
+++ 007/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+// By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+//
+// What is the 10 001st prime number?
+
+func main() {
+	var primeNumber, primeValue int
+
+	for {
+		primeValue++
+		if IsPrime(primeValue) {
+			primeNumber++
+		}
+		if primeNumber == 10001 {
+			break
+		}
+	}
+
+	fmt.Println(primeValue, " is prime number", primeNumber)
+}
+
+func IsPrime(value int) bool {
+	for i := 2; i <= int(math.Floor(float64(value)/2)); i++ {
+		if value%i == 0 {
+			return false
+		}
+	}
+	return value > 1
+}
