cf5ae3aaf4b22ca93e90d6aa4a297cdf000dd21f
Flemming Mahler <flemming@mahler.io>
2021-02-05 17:13:40 +0100

    Solved problem 81

---
+++ 081/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	data, err := ioutil.ReadFile("p081_matrix.txt")
+	if err != nil {
+		log.Fatal("File reading error", err)
+		return
+	}
+
+	fileContent := strings.Split(string(data), "\n")
+
+	var grid [80][80]int
+	for i := 0; i < 80; i++ {
+		numbers := strings.Split(fileContent[i], ",")
+		for j := 0; j < 80; j++ {
+			text := numbers[j]
+			grid[i][j], _ = strconv.Atoi(text)
+
+		}
+	}
+
+	//fmt.Println(grid)
+
+	for i := 78; i > -1; i-- {
+		grid[79][i] += grid[79][i+1]
+		grid[i][79] += grid[i+1][79]
+	}
+
+	for i := 78; i > -1; i-- {
+		for j := 78; j > -1; j-- {
+			if grid[i+1][j] < grid[i][j+1] {
+				grid[i][j] += grid[i+1][j]
+			} else {
+				grid[i][j] += grid[i][j+1]
+			}
+		}
+	}
+
+	fmt.Println("081/ Find the minimal path sum from the top left to the bottom right by only moving right and down")
+	fmt.Println(grid[0][0])
+}
