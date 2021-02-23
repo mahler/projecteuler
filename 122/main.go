5080ccfb6374ef43027087986277972fc959ac44
Flemming Mahler <flemming@mahler.io>
2021-02-23 17:05:34 +0100

    Solution to problem 122

--- 122/main.go
+++ 122/main.go
@@ -4,23 +4,81 @@ import (
 	"fmt"
 )
 
-// See https://projecteuler.net/problem=122
-func main() {
-
-	fmt.Println("122/ For 1 ≤ k ≤ 200, find ∑ m(k):")
-	fmt.Println(int(1 / prob(0, 0, 15)))
+type tree struct {
+	parent *tree
+	value  int
 }
 
-func prob(won, drawn, rounds int) float64 {
+const searchLength = 200 + 1
 
-	if drawn == rounds {
-		if 2*won > drawn {
-			return 1
-		} else {
-			return 0
+// The most naive way of computing n15 requires fourteen multiplications:
+//
+// n × n × ... × n = n15
+//
+// But using a "binary" method you can compute it in six multiplications:
+//
+// n × n = n^2
+// n^2 × n^2 = n^4
+// n^4 × n^4 = n^8
+// n^8 × n^4 = n^12
+// n^12 × n^2 = n^14
+// n^14 × n = n^15
+//
+// However it is yet possible to compute it in only five multiplications:
+//
+// n × n = n^2
+// n^2 × n = n^3
+// n^3 × n^3 = n^6
+// n^6 × n^6 = n^12
+// n^12 × n^3 = n^15
+//
+// We shall define m(k) to be the minimum number of multiplications to compute nk; for example m(15) = 5.
+//
+// For 1 ≤ k ≤ 200, find ∑ m(k).
+
+func main() {
+	table := [searchLength]int{}
+
+	haveseen := make(map[int]bool)
+	haveseen[1] = true
+	table[1] = 0
+
+	current := make([]*tree, 1)
+	current[0] = &tree{value: 1}
+
+	for i := 0; i < 14; i++ {
+
+		next := make([]*tree, 0)
+		for _, leaf := range current {
+			n := leaf.value
+
+			ancestor := leaf
+			temp := make([]*tree, 0)
+			for ancestor != nil {
+				consider := n + ancestor.value
+				if !haveseen[consider] {
+
+					if consider < searchLength {
+						table[consider] = i + 1
+
+					}
+					haveseen[consider] = true
+					temp = append([]*tree{{leaf, consider}}, temp...)
+				}
+				ancestor = ancestor.parent
+			}
+			next = append(next, temp...)
 		}
+
+		current = next
+
 	}
 
-	return prob(won+1, drawn+1, rounds)*1/float64(drawn+2) + prob(won, drawn+1, rounds)*float64(drawn+1)/float64(drawn+2)
+	total := 0
+	for i := 1; i < searchLength; i++ {
+		total += table[i]
+	}
 
+	fmt.Println("122/ For 1 ≤ k ≤ 200, find ∑ m(k)")
+	fmt.Println(total - 2)
 }
