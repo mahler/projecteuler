bf3c0e67f330fd3c26cbe8b5369ad1e8557b349f
Flemming Mahler <flemming@mahler.io>
2021-03-10 07:46:00 +0100

    Solution to problem 109

---
+++ 109/main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"fmt"
+)
+
+const target = 100
+
+func main() {
+	counter := 0
+
+	for i := 0; i <= 21; i++ {
+
+		for j := i; j <= 21; j++ {
+			if i == 21 {
+				i = 25
+			}
+			if j == 21 {
+				j = 25
+			}
+
+			for k := 1; k <= 21; k++ {
+				if k == 21 {
+					k = 25
+				}
+
+				for a := 1; a <= 3 && !(i == 0 && a > 1); a++ {
+					for b := a; b <= 3 && !(j == 0 && b > 1); b++ {
+						if !((b == 3 && j == 25) || (a == 3 && i == 25)) {
+
+							if (a*i)+(b*j)+(2*k) < target {
+
+								counter++
+							}
+						}
+					}
+				}
+
+			}
+		}
+	}
+
+	for i := 1; i <= 21; i++ {
+		if i == 21 {
+			i = 25
+		}
+
+		for j := i + 1; j <= 21; j++ {
+
+			if j == 21 {
+				j = 25
+			}
+
+			for k := 1; k <= 21; k++ {
+				if k == 21 {
+					k = 25
+				}
+				for b := 1; b <= 3; b++ {
+
+					for a := b + 1; a <= 3; a++ {
+						if !((b == 3 && j == 25) || (a == 3 && i == 25)) {
+							if (a*i)+(b*j)+(2*k) < target {
+
+								counter++
+							}
+
+						}
+
+					}
+				}
+
+			}
+		}
+	}
+
+	fmt.Println("109/ How many distinct ways can a player checkout with a score less than 100?")
+	fmt.Println(counter)
+}
